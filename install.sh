bc2f715b09d8ca9a1c91b8be570c78dc9dcde7a1
ktjjang <connjenio7@naver.com>
2022-02-02 06:05:34 +0000

    build vim & remove nvim
    Signed-off-by: ktjjang <connjenio7@naver.com>

--- install.sh
+++ install.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
-# before using this script, essential binaries must be installed in your system.
+# install dependnecy for build vim
+sudo apt-get install -y gcc make
 
-# install ripgrep using following command
-# sudo dpkg -i ripgrep_12.1.1_amd64.deb
-
-# NVIM_IMG_NAME=nvim_0_5_1.appimage
-rm -rf bundle
+# unzip and build vim 8.2 source code
 rm -rf vim
-tar xvf bundle.tar
 cat vim.tara* | tar xvf -
+cd vim/src
+./configure
+make -j4
+cd ../..
 
-# install nvim settings
-# mkdir -p $HOME/.config/nvim/autoload
-# mkdir -p $HOME/.config/nvim/site/autoload
-# cp pathogen.vim $HOME/.config/nvim/autoload/
-# cp init.vim $HOME/.config/nvim/
-# cp -r bundle $HOME/.local/share/nvim/site/
-
-# install nvim
-# chmod u+x ${NVIM_IMG_NAME} && ./${NVIM_IMG_NAME} --appimage-extract
-# ln -s $PWD/squashfs-root/AppRun $PWD/squashfs-root/vi
-# ln -s $PWD/squashfs-root/AppRun $PWD/squashfs-root/vim
+# unzip vim bundle
+rm -rf bundle
+tar xvf bundle.tar
 
 # install vim settings
+rm -rf ~/.vim
 mkdir -p ~/.vim/autoload
 cp pathogen.vim ~/.vim/autoload/pathogen.vim
 cp vimrc $HOME/.vimrc
 cp -r bundle $HOME/.vim/
 ln -s $PWD/vim/src/vim $PWD/vim/src/vi
 
+# install ~/.profile setting
 STR='[ -f $HOME/my-settings/my_profilerc ] && . $HOME/my-settings/my_profilerc'
 cat $HOME/.profile | grep -F my-settings
 if [ $? -ne 0 ];then
 	echo ".profile has not been included my_profilerc"
 	echo $STR >> $HOME/.profile
 fi
-# install bashrc setting
+# install ~/.bashrc setting
 STR='[ -f $HOME/my-settings/my_bashrc ] && . $HOME/my-settings/my_bashrc'
 cat $HOME/.bashrc | grep -F my-settings
 if [ $? -ne 0 ];then
 	echo ".bashrc has not been included my_bashrc"
 	echo $STR >> $HOME/.bashrc
 fi
+
